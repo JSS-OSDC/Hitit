7d946a228868d5f63aa12c93a2ffb0a1b532fcc1
kanishk sharma <kanishksharma3012@gmail.com>
2015-12-24 16:58:55 +0530

    added the target image

--- app/src/main/java/com/example/myhp/hitthebasket/Hitit.java
+++ app/src/main/java/com/example/myhp/hitthebasket/Hitit.java
@@ -12,15 +12,19 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.util.Random;
+
 public class Hitit extends Activity implements View.OnTouchListener{
     MyBringBackSurface ourSurfaceView;
     float x,y,sX,sY,fX,fY,dX,dY,aniX,aniY,scaledX,scaledY;
-    Bitmap test;
+
+    Bitmap test,plus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ourSurfaceView=new MyBringBackSurface(this);
         ourSurfaceView.setOnTouchListener(this);
+       // d=0;
         x=0;
         y=0;
         sX=0;
@@ -31,12 +35,13 @@ public class Hitit extends Activity implements View.OnTouchListener{
         aniY=0;
         scaledX=0;
         scaledY=0;
-        test= BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
-        // plus= BitmapFactory.decodeResource(getResources(),R.drawable.icon);
+        test= BitmapFactory.decodeResource(getResources(),R.drawable.greenball);
+        plus= BitmapFactory.decodeResource(getResources(),R.drawable.icon);
         setContentView(ourSurfaceView);
 
 
     }
+
 
     @Override
     protected void onPause() {
@@ -70,8 +75,8 @@ public class Hitit extends Activity implements View.OnTouchListener{
                 fY=event.getY();
                 dX=fX-sX;
                 dY=fY-sY;
-                scaledX=dX/30;
-                scaledY=dY/30;
+                scaledX=dX/20;
+                scaledY=dY/20;
                 break;
         }
         return true;
@@ -79,37 +84,65 @@ public class Hitit extends Activity implements View.OnTouchListener{
 
     class MyBringBackSurface extends SurfaceView implements Runnable{
         SurfaceHolder ourHolder;
+        Canvas canvas;
         Thread ourThread=null;
+        int d=0;
+        float f;
         boolean isRunning=false;
         public MyBringBackSurface(Context context) {
             super(context);
             ourHolder=getHolder();
             ourThread=new Thread(this);
             ourThread.start();
+
         }
 
+        public void  randomize(){
+
+                double db = Math.random();
+                f = (float) (db * (canvas.getWidth()-200));
+                d=1;
+
+        }
         @Override
         public void run() {
             while(isRunning)
             {
                 if(!ourHolder.getSurface().isValid())
                     continue;
-                Canvas canvas=ourHolder.lockCanvas();
-                canvas.drawRGB(2,2,150);
-                if(x!=0&&y!=0){
-                    canvas.drawBitmap(test,x-(test.getWidth()/2),y-(test.getHeight()/2),null);
+                canvas=ourHolder.lockCanvas();
+                canvas.drawRGB(2, 2, 150);
+
+if(d==0)
+    randomize();
+                    //canvas=ourHolder.lockCanvas();
+                    canvas.drawBitmap(plus,f,0,null);
+
+
+              if(x!=0&&y!=0){
+                 canvas.drawBitmap(test,x-(test.getWidth()/2),y-(test.getHeight()/2),null);
                 }
                 if(sX!=0&sY!=0){
                     //      canvas.drawBitmap(plus,sX-(plus.getWidth()/2),sY-(plus.getHeight()/2),null);
                 }
-                if(fX!=0&&fY!=0){
+               if(fX!=0&&fY!=0){
                     canvas.drawBitmap(test,fX-(test.getWidth()/2)-aniX,fY-(test.getHeight()/2)-aniY,null);
                     //      canvas.drawBitmap(plus,fX-(plus.getWidth()/2),fY-(plus.getHeight()/2),null);
-                }
+               }
+                check();
                 aniX=aniX+scaledX;
                 aniY=aniY+scaledY;
+
                 ourHolder.unlockCanvasAndPost(canvas);
+
             }
+        }
+
+        public void check(){
+            float z,w;
+            z=fX-(test.getWidth()/2)-aniX;
+            w=fY-(test.getHeight()/2)-aniY;
+          //                                  if(==||==)
         }
 
         public void pause(){
